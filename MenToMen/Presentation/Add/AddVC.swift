051faceb390315c8d97b819661d70076ca232526
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2022-10-19 19:42:49 +0900

    ⚡️Add post / alert
    +게시물 업로드 기능 추가
    +게시물 업로드 서버 연결 완료
    +게시물 업로드 알람 추가

--- MenToMen/Presentation/Add/AddVC.swift
+++ MenToMen/Presentation/Add/AddVC.swift
@@ -1,33 +1,144 @@
 import UIKit
 import Then
 import SnapKit
-
-fileprivate let url = URL(string: "\(API)/post/submit")
+import Alamofire
 
 class AddVC: UIViewController, ConstraintRelatableTarget {
     
+    var tag:String = ""
+    var data:PostDatas?
+    
     private let contentTextField = UITextView().then {
-//        $0.placeholder = " 멘토에게 부탁할 내용을 입력하세요."
         $0.font = UIFont(name:"Pretendard-Regular" , size: 22.0)
         $0.backgroundColor = UIColor(red: 0.95, green: 0.95, blue: 0.95, alpha: 1.0)
         $0.layer.cornerRadius = 20
     }
+    
+    private let stackView = UIStackView().then {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .systemBackground
+        $0.distribution = .fillEqually
+    }
+    
+    private let designButton = UIButton().then {
+        $0.setImage(UIImage(named: "B1"), for: .normal)
+        $0.addTarget(self, action: #selector(tabDesignButton), for: .touchUpInside)
+    }
+    @objc func tabDesignButton() { tag = "DESIGN" }
+    
+    private let webButton = UIButton().then {
+        $0.setImage(UIImage(named: "B2"), for: .normal)
+        $0.addTarget(self, action: #selector(tabWebButton), for: .touchUpInside)
+    }
+    @objc func tabWebButton() { tag = "WEB" }
+    
+    private let serverButton = UIButton().then {
+        $0.setImage(UIImage(named: "B3"), for: .normal)
+        $0.addTarget(self, action: #selector(tabServerButton), for: .touchUpInside)
+    }
+    @objc func tabServerButton() { tag = "SERVER" }
+    
+    private let androidButton = UIButton().then {
+        $0.setImage(UIImage(named: "B4"), for: .normal)
+        $0.addTarget(self, action: #selector(tabAndroidButton), for: .touchUpInside)
+    }
+    @objc func tabAndroidButton() { tag = "ANDROID" }
+    
+    private let iOSButton = UIButton().then {
+        $0.setImage(UIImage(named: "B5"), for: .normal)
+        $0.addTarget(self, action: #selector(tabiOSButton), for: .touchUpInside)
+    }
+    @objc func tabiOSButton() { tag = "IOS" }
+    
+    
     private let addPostButton = UIButton().then {
         $0.backgroundColor = UIColor(red: 0.2549, green: 0.3608, blue: 0.949, alpha: 1.0)
         $0.setTitle("멘토 요청하기", for: .normal)
         $0.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 16.0)
         $0.titleLabel?.textColor = .black
         $0.titleLabel?.textAlignment = .center
-        $0.layer.cornerRadius = 20.0
+        $0.layer.cornerRadius = 4.0
+        $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        $0.layer.masksToBounds = true
         $0.addTarget(self, action: #selector(TabAddPostButton), for: .touchUpInside)
     }
     
+    func submit(_ params: [String: Any]) {
+        AF.request("\(API)/post/\(data == nil ? "submit" : "update")",
+                   method: data == nil ? .post : .patch,
+                   parameters: params,
+                   encoding: JSONEncoding.default,
+                   headers: ["Content-Type": "application/json"],
+                   interceptor: Requester()
+        ) { $0.timeoutInterval = 5 }
+            .validate()
+            .responseData { response in
+                print(params)
+                checkResponse(response)
+                switch response.result {
+                case .success:
+                    print("POST 성공")
+                    self.postAlert()
+                case .failure(let error):
+                    print(error)
+                }
+            }
+    }
+    
+    func postAlert() {
+        let alert = UIAlertController(title: "성공!", message: "멘토 요청이 성공적으로 되었습니다!", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: {_ in
+            let VC = TabBarController()
+            VC.modalPresentationStyle = .fullScreen
+            self.present(VC, animated: true, completion: nil) }))
+        present(alert, animated: true)
+    }
+    
     @objc func TabAddPostButton() {
+        var reqParam: [String: Any] = ["content": contentTextField.text!, "imgUrls": []]
+        reqParam["tag"] = tag
+        if data != nil {
+            reqParam["postId"] = data!.postId
+        }
+        submit(reqParam)
+    }
+    
+    private func addKeyboardNotification() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            addPostButton.frame.origin.y -= keyboardHeight
+        }
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        if let keyboardFrame: NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+            let keybaordRectangle = keyboardFrame.cgRectValue
+            let keyboardHeight = keybaordRectangle.height
+            addPostButton.frame.origin.y += keyboardHeight
+        }
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        addKeyboardNotification()
         setupNavigationBar()
         setUp()
         
@@ -38,15 +149,30 @@ class AddVC: UIViewController, ConstraintRelatableTarget {
         
         [
             addPostButton,
+            stackView,
             contentTextField
             
         ].forEach{ self.view.addSubview($0) }
+        
+        
+        stackView.addArrangedSubview(designButton)
+        stackView.addArrangedSubview(webButton)
+        stackView.addArrangedSubview(androidButton)
+        stackView.addArrangedSubview(serverButton)
+        stackView.addArrangedSubview(iOSButton)
         
         contentTextField.snp.makeConstraints {
             $0.top.equalToSuperview().offset(100)
             $0.left.equalToSuperview().offset(14)
             $0.right.equalToSuperview().offset(-14)
             $0.bottom.equalTo(contentTextField.snp.top).offset(100)
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.top.equalTo(contentTextField.snp.bottom).offset(8)
+            $0.left.equalToSuperview().offset(4)
+            $0.right.equalToSuperview().offset(-4)
+            $0.bottom.equalTo(stackView.snp.top).offset(22)
         }
         
         addPostButton.snp.makeConstraints {
--- MenToMen/Presentation/Cell.swift
+++ MenToMen/Presentation/Cell.swift
@@ -2,84 +2,84 @@ import UIKit
 import SnapKit
 import Then
 
-class CustomCell: UITableViewCell {
-    
+class Cell: UITableViewCell {
+
     static let identifier = "CustomCell"
-    
-    let tagImage = UIImageView().then {
+
+    private let tagImage = UIImageView().then {
         $0.translatesAutoresizingMaskIntoConstraints = false
     }
-    
-    let userName = UILabel().then {
+
+    private let userName = UILabel().then {
         $0.text = "OOO"
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.font = UIFont(name: "Pretendard-Regular", size: 18.0)
     }
-    
-    let userInfo = UILabel().then {
+
+    private let userInfo = UILabel().then {
         $0.text = "O학년 O반 O번"
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.font = UIFont(name: "Pretendard-ExtraLight", size: 12.0)
     }
-    
-    let content = UILabel().then {
+
+    private  let content = UILabel().then {
         $0.text = "내용"
         $0.translatesAutoresizingMaskIntoConstraints = false
         $0.font = UIFont(name: "Pretendard-Light", size: 15.0)
         $0.numberOfLines = 2
     }
-    
-    let postImage = UIImageView().then {
+
+    private let postImage = UIImageView().then {
         $0.layer.cornerRadius = 4.0
         $0.backgroundColor = .systemBackground
     }
-    
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 16.0, left: 16, bottom: 16, right: 16))
 
         backgroundColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.15)
         layer.borderWidth = 0.25
-        
+
         autoLayout()
     }
-    
+
     private func autoLayout() {
-        
+
         [
             tagImage,
             userName,
             userInfo,
             content,
             postImage
-            
+
         ].forEach{ self.contentView.addSubview($0) }
-        
+
         tagImage.snp.makeConstraints {
             $0.top.equalToSuperview()
             $0.left.equalToSuperview().offset(16)
             $0.right.equalTo(self.snp.left).offset(44)
             $0.bottom.equalToSuperview().offset(-58)
         }
-        
+
         userName.snp.makeConstraints {
-            $0.top.equalToSuperview().offset(6)
+            $0.top.equalToSuperview()
             $0.left.equalTo(tagImage.snp.right).offset(10)
             $0.right.equalToSuperview().offset(-100)
         }
-        
+
         userInfo.snp.makeConstraints {
             $0.top.equalTo(userName.snp.bottom).offset(2)
             $0.left.equalTo(tagImage.snp.right).offset(10)
         }
-        
+
         content.snp.makeConstraints {
             $0.top.equalToSuperview().offset(10)
             $0.left.equalToSuperview().offset(20)
             $0.right.equalTo(tagImage.snp.right).offset(240)
             $0.bottom.equalToSuperview().offset(16)
         }
-        
+
         postImage.snp.makeConstraints {
             $0.top.equalToSuperview().offset(8)
             $0.left.equalTo(content.snp.right).offset(16)
@@ -87,9 +87,9 @@ class CustomCell: UITableViewCell {
             $0.bottom.equalToSuperview().offset(-8)
         }
     }
-    
+
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
 }
